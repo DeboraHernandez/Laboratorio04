0471da90eafca9979dca44cd1844f48efc51d88c
Débora Hernández <debora.hgk6@gmail.com>
2023-03-18 12:27:00 -0500

    Realizar ejercicio 1

--- Laboratorio04/Program.cs
+++ Laboratorio04/Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Nombre: ");
-            Console.WriteLine("Carnet: ");
-            Console.WriteLine("Seccion: ");
+            Console.WriteLine("Nombre: Débora Hernández");
+            Console.WriteLine("Carnet: 1186023");
+            Console.WriteLine("Seccion: 04");
         }
     }
 }
