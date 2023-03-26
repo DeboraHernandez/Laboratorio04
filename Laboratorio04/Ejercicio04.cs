f74b32004f8f1426939928425aa630866807de23
Débora Hernández <debora.hgk6@gmail.com>
2023-03-25 22:58:11 -0500

    Realizar ejercicio 4

--- Laboratorio04/Ejercicio04.cs
+++ Laboratorio04/Ejercicio04.cs
@@ -8,14 +8,14 @@ namespace Laboratorio04
 {
     public class Ejercicio04
     {
-        public int[] CalcularBilletes(int cantidad, int[] billetes)
+        public int[] CalcularBilletes(int cant, int[] billetes)
         {
             int[] result = new int[billetes.Length];
 
             for (int i = 0; i < billetes.Length; i++)
             {
-                result[i] = cantidad / billetes[i];
-                cantidad = cantidad % billetes[i];
+                result[i] = cant / billetes[i];
+                cant = cant % billetes[i];
             }
             return result;
         }
